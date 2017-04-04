5ef28d9a3302e3d11e2c2f23e4a3f1d47e29ff2e
Rosie Gollancz <r.gollancz@gmail.com>
2017-04-04 15:23:59 +0100

    Adds animation to lines and Math to marker angle. Fixes map to page with text scrolling

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var width = window.innerWidth,
     height = window.innerHeight;
 
 var projection = d3.geoMercator()
-    .translate([width / 2, height / 2])
-    .scale((width - 1) / 2 / Math.PI);
+    .translate([width / 2, height / 2 - 100])
+    .scale((width - 1.3) / 2 / Math.PI);
 
 var path = d3.geoPath()
         .projection(projection);
 
 var svgWorldMap = d3.select('#world-map').append('svg')
     .attr('width', width)
-    .attr('height', height);
+    .attr('height', height * 0.5);
 
 d3.json("./data/world-50m.json", function(error, worldMap) {
     if (error) throw error;
@@ -44,6 +44,10 @@ d3.json("./data/world-50m.json", function(error, worldMap) {
         var projectedDestination = projection([destinationLongitude, destinationLatitude]);
         var color = colors[i];
 
+        var dx = projectedDestination[0] - projectedOrigin[0];
+        var dy = projectedDestination[1] - projectedOrigin[1];
+        var arcTangent = Math.atan2(dy, dx);
+
         var defs = g.append('defs')
             .append('marker')
                 .attr('id', 'arrow')
@@ -51,18 +55,26 @@ d3.json("./data/world-50m.json", function(error, worldMap) {
                   .attr('markerHeight', 10)
                   .attr('refX', 0)
                   .attr('refY', 3)
-                  .attr('orient', 'auto')
+                  .attr('orient', arcTangent * (180 / Math.PI))
                   .attr('markerUnits', 'strokeWidth')
                       .append('path')
                           .attr('d', 'M0,0 L0,5 L9,3 z')
-                          .attr('fill', color);
+                          .attr('fill', color)
+                          .attr('opacity', 0.8);
+
 
         line.attr('x1', projectedOrigin[0])
             .attr('y1', projectedOrigin[1])
-            .attr('x2', projectedDestination[0])
-            .attr('y2', projectedDestination[1])
+            .attr('x2', projectedOrigin[0])
+            .attr('y2', projectedOrigin[1])
             .attr('stroke', color)
-            .attr('marker-end', 'url(#arrow)');
+            .attr('marker-end', 'url(#arrow)')
+            .attr('opacity', 0.8).transition()
+                .duration(500)
+                .ease(d3.easeCubicOut)
+                .delay(500 + (200 * i))
+                .attr('x2', projectedDestination[0])
+                .attr('y2', projectedDestination[1])
     }
   })
 
